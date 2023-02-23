f560f89d750d4eb609702ba42ab6de56c1e9e0b6
Ysunil9 <sunil.pcs24@gmail.com>
2023-02-23 15:34:55 +0530

    [SUNIL]:Product review list is added

---
+++ ManageReview.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_ProductReviewManagement
+{
+    public class ManageReview
+    {
+        public void Display(List<ProductReviewModel> reviewList)
+        {
+            Console.WriteLine("Display The FeedBack of The Product\n");
+            //Print products
+            foreach (var feedback in reviewList)
+            {
+                Console.WriteLine($"ProductID: {feedback.ProductId},UserId: {feedback.UserId}Rating: {feedback.Rating},Review: {feedback.Review}");
+            }
+        }
+    }
+}
